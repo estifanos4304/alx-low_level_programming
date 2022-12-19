3d0980466eacc39244d99fa4cdc96173bf94b40a
estifanos4304 <estiflisan@gmail.com>
2022-12-19 05:06:14 -0800

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101_test.c
@@ -0,0 +1,80 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Function to randomly generates password
+ * @N: of length N
+ * Return: always 0
+ */
+
+void randomPasswordGeneration(int N)
+ {
+	 int i = 0;
+	 int randomizer = 0;
+
+	 /* Seed the random-number generator
+	  * with current time so that the
+	  * numbers will be different every time
+	  */
+
+	 srand((unsigned int)(time(NULL)));
+	 char numbers[] = "0123456789";
+
+	 /*Array of small alphabets*/
+
+	 char letter[] = "abcdefghijklmnoqprstuvwyzx";
+
+	 /*Array of capital alphabets*/
+
+	 char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+	 /*Array of all the special symbols*/
+
+	 char symbols[] = "!@#$^&*?";
+	  char password[N];
+	  
+	  /*inside the loop*/
+
+	  randomizer = rand() % 4;
+
+	  /*Iterate over the range [0, N]*/
+
+	  for (i = 0; i < N; i++)
+	  {
+		  if (randomizer == 1)
+		  {
+			  password[i] = numbers[rand() % 10];
+			  randomizer = rand() % 4;
+			  printf("%c", password[i];
+					  }
+					  else if (randomizer == 2)
+					  {
+					  password[i] = symbols[rand() % 8];
+					  randomizer = rand() % 4;
+					  printf("%c", password[i]);
+					  }
+					  else if (randomizer == 3)
+					  {
+					  password[i] = LETTER[rand() % 26];
+					  randomizer = rand() % 4;
+					  printf("%c", password[i]);
+					  }
+					  else
+					  {
+					  password[i] = letter[rand() % 26];
+					  randomizer = rand() % 4;
+					  printf("%c", password[i]);
+					  }
+					  }
+ }
+int main(void)
+{
+	/* Length of the password to be generated*/
+	int N = 10;
+	/*Function Call*/
+	randomPasswordGeneration(N);
+	return (0);
+}
+
