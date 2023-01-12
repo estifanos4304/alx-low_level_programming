4add80ca777d8165cdbf45901bb71aeb1fe449b2
Estifanos4304 <estiflisan@gmail.com>
2023-01-12 18:55:30 +0300

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -6,3 +10,10 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+int main(int argc, char const *argv[]);
+void print_int(unsigned long int n);
+int _atoi(const char *s);
+void _puts(char *str);
+#endif
