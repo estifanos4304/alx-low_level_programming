f3005dad1b458333c79dfd32c313f9719e90965f
estifanos4304 <estiflisan@gmail.com>
2023-01-31 02:36:59 -0800

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,25 +9,16 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
+	const listint_t *aux_node = head;
 
-	if (head == NULL)
+	if (!head)
 		exit(98);
-
-	while (one && two && two->next && head)
+	while (aux_node)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
+		aux_node = aux_node->next;
 		nodes++;
 	}
-	head = NULL;
 	return (nodes);
+
 }
